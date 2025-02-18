61ed2f44049e0233ccda83d06dc825a7c124d0bf
Ori Pomerantz <qbzzt1@gmail.com>
2025-02-17 21:26:49 -0600

    Make running easier

--- packages/server/deployVerifier.sh
+++ packages/server/deployVerifier.sh
@@ -1,5 +1,5 @@
-#! /usr/bin/sh
+#! /bin/sh
 
 PRIVATE_KEY=`cat ../contracts/.env | grep PRIVATE_KEY | sed 's/PRIVATE_KEY=//'`
 cd ../contracts
-echo `forge create Verifier --private-key $PRIVATE_KEY | awk '/Deployed to: / {print $3} '`
+echo `forge create Verifier --private-key $PRIVATE_KEY --broadcast | awk '/Deployed to: / {print $3} '`
